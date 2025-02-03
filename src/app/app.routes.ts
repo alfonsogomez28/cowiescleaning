65532bc2309756f479f3103d42ad99ab3d03e13c
Fonso <fonso@alfonsos-mini.lan>
2025-02-03 12:17:10 -0700

    added new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, RouterLink } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home on empty path
+    { path: '**', redirectTo: '/home' } // Redirect to home on unknown path
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
